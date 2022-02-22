28e38c7d0d45a59768010aceaf1bd8fe259a0546
MOHD OWAIS <mohd.owais@publicissapient.com>
2022-02-22 18:00:15 +0530

    My first assesement

---
+++ src/main/java/sample/test/App.java
@@ -0,0 +1,16 @@
+package sample.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 String str1="Owais "; 
+         String str2="Siddiqui";
+         System.out.println("Concatenated String is ");
+         System.out.println("Hello "+str1.concat(str2));
+     }
+    }
